2c49aa65dfade58262fb653306ff31e4342e598e
sleonia <sleonia@student.21-school.ru>
2020-01-18 20:14:21 +0300

    Protect plateau and update code style

--- sources/parse_plateau.c
+++ sources/parse_plateau.c
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 01:59:32 by sleonia           #+#    #+#             */
-/*   Updated: 2020/01/18 19:46:57 by sleonia          ###   ########.fr       */
+/*   Updated: 2020/01/18 20:13:28 by sleonia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static bool	parse_plateau_size(t_env *env)
 	if (ft_len_arr(split) < 3)
 	{
 		ft_destroy_string_arr(split);
-		return (false);			
+		return (false);
 	}
 	env->plateau->height = ft_atoi(split[1]);
 	env->plateau->width = ft_atoi(split[2]);
@@ -38,13 +38,8 @@ static bool	parse_plateau_size(t_env *env)
 	return (true);
 }
 
-static bool	read_plateau_map(t_env *env)
+static bool	read_plateau_loop(int i, char *line, char **split, t_env *env)
 {
-	int		i;
-	char	*line;
-	char	**split;
-
-	i = -1;
 	while (++i < env->plateau->height)
 	{
 		if (get_next_line(0, &line) != 1)
@@ -58,7 +53,7 @@ static bool	read_plateau_map(t_env *env)
 		if (ft_len_arr(split) < 2)
 		{
 			ft_destroy_string_arr(split);
-			return (false);			
+			return (false);
 		}
 		if (!(env->plateau->map[i] = ft_strdup(split[1])))
 		{
@@ -68,6 +63,15 @@ static bool	read_plateau_map(t_env *env)
 		ft_destroy_string_arr(split);
 	}
 	return (true);
+}
+
+static bool	read_plateau_map(t_env *env)
+{
+	int		i;
+	char	*line;
+	char	**split;
+
+	return (read_plateau_loop(-1, line, split, env));
 }
 
 bool		parse_plateau(t_env *env)
@@ -82,6 +86,9 @@ bool		parse_plateau(t_env *env)
 	if (!(env->plateau->map = init_map(env->plateau->height + 1)))
 		return (false);
 	if (!read_plateau_map(env))
+	{
+		ft_destroy_string_arr(env->plateau->map);
 		return (false);
+	}
 	return (true);
 }
